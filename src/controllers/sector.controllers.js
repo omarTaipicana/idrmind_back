45d52f6759c3ae567bdda51724496f2244a05772
admin <fernandoparadoja@gmail.com>
2026-08-12 00:04:06 -0500

    sector

--- src/controllers/sector.controllers.js
+++ src/controllers/sector.controllers.js
@@ -1,32 +1,32 @@
 const catchError = require('../utils/catchError');
-const sector = require('../models/sector');
+const Sector = require('../models/Sector');
 
 const getAll = catchError(async(req, res) => {
-    const results = await sector.findAll();
+    const results = await Sector.findAll();
     return res.json(results);
 });
 
 const create = catchError(async(req, res) => {
-    const result = await sector.create(req.body);
+    const result = await Sector.create(req.body);
     return res.status(201).json(result);
 });
 
 const getOne = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await sector.findByPk(id);
+    const result = await Sector.findByPk(id);
     if(!result) return res.sendStatus(404);
     return res.json(result);
 });
 
 const remove = catchError(async(req, res) => {
     const { id } = req.params;
-    await sector.destroy({ where: {id} });
+    await Sector.destroy({ where: {id} });
     return res.sendStatus(204);
 });
 
 const update = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await sector.update(
+    const result = await Sector.update(
         req.body,
         { where: {id}, returning: true }
     );
